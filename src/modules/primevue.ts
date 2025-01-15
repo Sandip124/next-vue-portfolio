7f86c090c5b123dd32bcc4339186045077647897
Sandip Chaudhary <37034590+Sandip124@users.noreply.github.com>
2025-01-15 14:02:17 +0100

    update color

--- src/modules/primevue.ts
+++ src/modules/primevue.ts
@@ -13,17 +13,17 @@ const MyPreset = definePreset(Aura, {
   semantic: {
     primary: {
       0: '#fff',
-      50: '{purple.50}',
-      100: '{purple.100}',
-      200: '{purple.200}',
-      300: '{purple.300}',
-      400: '{purple.400}',
-      500: '{purple.500}',
-      600: '{purple.600}',
-      700: '{purple.700}',
-      800: '{purple.800}',
-      900: '{purple.900}',
-      950: '{purple.950}',
+      50: '{slate.50}',
+      100: '{slate.100}',
+      200: '{slate.200}',
+      300: '{slate.300}',
+      400: '{slate.400}',
+      500: '{slate.500}',
+      600: '{slate.600}',
+      700: '{slate.700}',
+      800: '{slate.800}',
+      900: '{slate.900}',
+      950: '{slate.950}',
     },
     colorScheme: {
       light: {
